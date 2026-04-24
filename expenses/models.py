739fb321de8715a85b6adedfd5d9a533a78e1e4b
Ibrahim Tawfeeq Hamdouna <ibrahimhamdouna11@gmail.com>
2026-04-24 16:40:54 +0300

    Finised exFinished Expense models

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    salary = models.DecimalField(
+        max_digits=13, 
+        decimal_places=2, 
+        null=True, 
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='categories'
+    )
+
+    class Meta():
+        constraints = [models.UniqueConstraint(
+            fields=['name', 'user'],
+            name='unique_category_per_user'
+        )]
+
+    def __str__(self):
+        return self.name
+
+class Expenses(models.Model):
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='expenses'
+    )
+    title = models.CharField(max_length=255)
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)]
+    ) 
+    date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.amount}"
--- my_expenses/settings.py
+++ my_expenses/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'expenses.apps.ExpensesConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +76,16 @@ WSGI_APPLICATION = 'my_expenses.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_expenses',     
+        'USER': 'root',               
+        'PASSWORD': '2003',  
+        'HOST': '127.0.0.1',          
+        'PORT': '3306',               
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -115,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'expenses.User'
